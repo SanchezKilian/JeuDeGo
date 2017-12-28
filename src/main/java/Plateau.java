be44ba26523ce946235688f7672e75665fe8df5c
Kilian sanchez <kilian0925@gmail.com>
2017-12-28 11:53:43 +0100

    Ajout fct calcul du score v1

--- src/main/java/Plateau.java
+++ src/main/java/Plateau.java
@@ -5,6 +5,8 @@ public class Plateau {
 	private Piece[][] plateau = new Piece[20][20];
 	private Piece newP;
 	
+	private int Pb = 0;
+	private int Pn = 0;
 	
 	public Plateau() {
 		for (int a = 0; a < 20 ; a++) {
@@ -21,9 +23,9 @@ public class Plateau {
 	}
 	
 	public void afficherP() {
-		for (int q = 0; q < 20; q++) {
+		for (int q = 1; q < 20; q++) {
 			System.out.print("\n");
-			for (int s = 0; s <20; s++) {
+			for (int s = 1; s <20; s++) {
 				
 				if (plateau[q][s].getClr() == 0) {
 					System.out.print("B");
@@ -95,6 +97,77 @@ public class Plateau {
 		
 		return CaseCheck.size();		
 		}
+	
+	
+	public int CalcScore() {  // i = pt pour les blancs  l = pt pour les noires.
+		boolean fin = false;
+		for(int a = 0; a < 20 ; a++) {
+			for (int b = 0; b <20; b++) {
+				if (plateau[a][b].getClr() == -1) {
+					while (!fin) {
+						
+					
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	public int Calcul(int X, int Y) {
+		int dessus =-15, dessous = -20,droite = -25 , gauche = -30;
+		if(X != 1) {
+			if (plateau[X-1][Y].getClr() != -1) {
+				return plateau[X-1][Y].getClr();
+			}else {
+				dessus = Calcul(X-1,Y);
+			}
+		}else {
+			return -9;
+		}
+		if(X != 19) {
+			if (plateau[X+1][Y].getClr() != -1) {
+				return plateau[X+1][Y].getClr();
+			}else {
+				dessous = Calcul(X+1,Y);
+			}
+		}else {
+			return -9;
+		}
+		if(Y != 1) {
+			if (plateau[X][Y-1].getClr() != -1) {
+				return plateau[X][Y-1].getClr();
+			}else {
+				gauche = Calcul(X,Y-1);
+			}
+		}else {
+			return -9;
+		}
+		if(Y != 19) {
+			if (plateau[X][Y+1].getClr() != -1) {
+				return plateau[X][Y+1].getClr();
+			}else {
+				droite = Calcul(X,Y+1);
+			}
+		}else {
+			return -9;
+		}
+		
+		if(dessus == dessous && gauche == droite && dessus == droite) {
+			if(dessus == 0) {
+				Pb ++;
+				System.out.println("pB++");
+			}
+			if(dessus == 1) {
+				Pn ++;
+				System.out.println("pN++");
+			}
+		}
+		
+		
+		return -10;
+		
+	}
 		
 	
 	
