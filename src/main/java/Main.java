d411c8c8d728f1170fe5c10aa62de8de05bb07eb
Kilian sanchez <kilian0925@gmail.com>
2017-12-30 15:51:27 +0100

    suppretion des test dans le main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		int choix = 0;
 		int posX;
 		int posY;
-		/*
+		
 		while (nbPasse !=2) {
 			System.out.println("joueur " + NJoueur + " : " );
 			System.out.println("\tPour jouer : 1 \n\tPour passer : 2 ");
@@ -85,41 +85,8 @@ public class Main {
 			P.afficherP();
 			
 		}	
-		*/
-		P.PlacerPiece(1, 1, 1);	
-		P.PlacerPiece(1, 2, 1);	
-		P.PlacerPiece(1, 3, 1);	
-		P.PlacerPiece(2, 1, 1);	
-		P.PlacerPiece(3, 1, 1);	
-		P.PlacerPiece(4, 1, 1);	
-		P.PlacerPiece(3, 4, 1);	
-		P.PlacerPiece(1, 4, 1);	
-		P.PlacerPiece(2, 4, 1);	
-		P.PlacerPiece(4, 3, 1);	
-		P.PlacerPiece(4, 2, 1);	
-		//P.PlacerPiece(2, 3, 1);
-		P.PlacerPiece(5, 6, 1);	
-		P.PlacerPiece(5, 4, 1);	
-		P.PlacerPiece(4, 5, 0);	
-		P.PlacerPiece(6, 5, 0);	
 		
-		P.PlacerPiece(7, 5, 0);	
-		P.PlacerPiece(8, 5, 0);	
-		P.PlacerPiece(9, 5, 0);	
-		P.PlacerPiece(10, 5, 0);	
-		P.PlacerPiece(11, 5, 0);	
-		P.PlacerPiece(12, 5, 0);	
-		P.PlacerPiece(13, 5, 0);	
-		P.PlacerPiece(14, 5, 0);	
-		
-		P.PlacerPiece(15, 5, 0);	
-		P.PlacerPiece(16, 5, 0);	
-		P.PlacerPiece(17, 5, 0);	
-		P.PlacerPiece(18, 5, 0);	
-		P.PlacerPiece(19, 5, 0);	
-		P.PlacerPiece(6, 6, 0);	
-		P.PlacerPiece(6, 7, 0);	
-		P.PlacerPiece(6, 8, 0);	
+	
 		
 		P.afficherP();
 		System.out.println(allCh.size());
